8b55185d830fc4cb9fc1f7100db2a4ec86007255
Knut Schleßelmann <knut.schlesselmann@fortis-it.de>
2019-07-24 14:11:49 +0200

    test: remove OAuth test configuration

--- src/test/kotlin/de/dlh/lht/checkin/users/IntegrationTest.kt
+++ src/test/kotlin/de/dlh/lht/checkin/users/IntegrationTest.kt
@@ -1,12 +1,9 @@
 package de.dlh.lht.checkin.users
 
-import de.otto.prada.metadata.configuration.OAuth2TestConfiguration
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.context.annotation.Import
 import org.springframework.test.context.ActiveProfiles
 
 @Target(AnnotationTarget.CLASS)
 @SpringBootTest
 @ActiveProfiles("test")
-@Import(OAuth2TestConfiguration::class)
 annotation class IntegrationTest
